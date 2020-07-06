c3830146cbb9273918ed3b62ce0ce804a9a181fa
Shankar Mohan <Shankar.Mohan@mrisoftware.com>
2020-07-06 14:06:59 -0400

    First commit for asp.net core sample application.

---
+++ sampleapp/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleService.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class HelloController : ControllerBase
+  {
+    private readonly ILogger<HelloController> _logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok("Hello!");
+    }
+  }
+}
